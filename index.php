8ac97fd5e438edf8d99c5eae593ac6ea1c88142c
mcforsas <karolis.poviliunas@gmail.com>
2019-05-20 18:31:16 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            print 'Hello world!';
+            if(1 == 1){
+                print_r("hello my babe");
+            }
+        ?>
+    </body>
+</html>
